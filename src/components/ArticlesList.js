b7efc514272a47ed92065332b55a88a29d043bf7
NatiFekadu <natifekadu808@gmail.com>
2023-02-04 11:47:41 +0300

    Article List

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import articles from '../pages/article-content'
+//import articles from '../pages/article-content'
 import {Link} from 'react-router-dom'
 
 const ArticlesList = ({ articles }) => {
--- src/pages/ArticleListPage.js
+++ src/pages/ArticleListPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+//import {Link} from 'react-router-dom';
 import articles from './article-content';
 import ArticlesList from '../components/ArticlesList';
 
